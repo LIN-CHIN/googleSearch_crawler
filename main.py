46cc0e1dfb27b0c717e616857e7a07c0935232fa
twtrubiks <s235068267@yahoo.com.tw>
2019-03-21 22:49:48 +0800

    First completed

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from urllib.request import urlopen,Request
+from bs4 import BeautifulSoup
+google_url = "https://www.google.com/search?q=python"
+r = Request(google_url)
+r.add_header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)")
+response = urlopen(r)
+soup = BeautifulSoup(response,'html.parser')
+res = soup.find_all("div",class_="g")
+x = 0
+
+for i in res :
+        x+=1
+        div = i.find("div",class_="hJND5c") #提取所有div
+        if div != None:
+            print("第", x, "筆")
+            cite = div.find("cite")
+            print(cite.text)
+        else :
+            x -= 1
+#----------
+#---test---
+#----------
+# print(res[3])
+# a = res[3].find("div",class_="hJND5c")
+# print(a)
+# if a!=None:
+#     b = a.find("cite")
+#     print(b)
+#     print(b.text)
+#else :
+#   print("is none")
